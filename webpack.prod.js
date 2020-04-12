8d758b0d21a05d8f7770f0ed3a06459840b8d5be
Souradip Mookerjee <sm2030@cam.ac.uk>
2020-04-12 01:41:10 +0100

    fix webpack eslint issues

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,7 @@
-// Learn more about this file at:
-// https://victorzhou.com/blog/build-an-io-game-part-1/#2-builds--project-setup
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
